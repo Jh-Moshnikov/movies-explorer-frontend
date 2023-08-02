5496931a6eb1e342e24e3fdc3e0ab723aef3cb2b
Jh_Moshnikov <Jh.Moshnikov@yandex.ru>
2023-08-03 02:56:42 +0300

    сверстан main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Promo from '../Promo/Promo';
+import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import './Main.css';
+
+
+function Main() {
+    return (
+        <main className='main'>
+            <Promo />
+            <NavTab />
+            <AboutProject />
+           
+        </main>
+
+);
+}
+
+export default  Main;
