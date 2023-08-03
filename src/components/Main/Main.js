059b494e492db0c0ba91a1465604a8b703ef7b1c
Jh_Moshnikov <Jh.Moshnikov@yandex.ru>
2023-08-03 23:31:47 +0300

    сверстано на разрешении 1280

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,9 @@ import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
+import Footer from '../Footer/Footer';
 import './Main.css';
 
 
@@ -12,7 +15,10 @@ function Main() {
             <Promo />
             <NavTab />
             <AboutProject />
-           
+            <Techs />
+            <AboutMe />
+            <Portfolio />
+            <Footer />
         </main>
 
 );
